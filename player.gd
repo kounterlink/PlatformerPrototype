e97fe3c865ef0be1820994f552d8e6a6c030c9b6
linkin <89432137+kounterlink@users.noreply.github.com>
2025-11-15 13:59:24 -0500

    variable jump height and gravity

--- player.gd
+++ player.gd
@@ -6,9 +6,11 @@ var state = States.IDLE
 
 @export var speed = 300
 @export var maxSpeed = 350
-@export var jumpVelocity = -700
+var jumpVelocity = -700
 @export var jumpLimit = 1
 const GRAVITY = 1200
+const MAX_GRAVITY = 1300
+var dynamicGravity = GRAVITY
 var jumpCount = 0 
 var canJump = true
 
@@ -31,19 +33,24 @@ func _physics_process(delta):
 			roll()
 
 	if (state != States.POUNDING):
-		if (Input.is_action_just_pressed('ROLL')):
+		if (Input.is_action_just_pressed('ROLL') and state != States.ROLLING):
 			roll()
 		
 		# apply gravity :)
 		if (not is_on_floor()):
 			canJump = false
-			velocity.y += GRAVITY * delta
+			if (dynamicGravity < MAX_GRAVITY):
+				dynamicGravity = MAX_GRAVITY
+
+			velocity.y += dynamicGravity * delta
 
 			if (velocity.y > 0):
+				dynamicGravity *= 1.05
 				state = States.FALLING
 		else:
 			# landing
 			canJump = true
+			dynamicGravity = GRAVITY
 			if (state == States.FALLING or state == States.JUMPING):
 				jumpCount = 0
 				if (direction == 0):
@@ -57,6 +64,8 @@ func _physics_process(delta):
 
 			if Input.is_action_just_pressed('JUMP'):
 				jump()
+			if Input.is_action_just_released('JUMP') and velocity.y < 0:
+				velocity.y = max(velocity.y, jumpVelocity * 0.4)
 
 			# doing actual movement
 
@@ -64,10 +73,12 @@ func _physics_process(delta):
 				if (state != States.JUMPING and state != States.FALLING):
 					state = States.WALKING
 				velocity.x = move_toward(velocity.x, direction * speed, maxSpeed * delta)
+
+				$Sprite2D.flip_h = direction < 0
 			else:
 				if (state != States.JUMPING and state != States.FALLING):
 					state = States.IDLE
-				velocity.x = move_toward(velocity.x, 0, maxSpeed * 2.0 * delta)
+				velocity.x = move_toward(velocity.x, 0, maxSpeed * 2.5 * delta)
 	# this needs to be here for everything to work xd
 
 	move_and_slide()
@@ -82,5 +93,12 @@ func jump():
 		velocity.y = jumpVelocity
 
 func roll():
-	# didn't add this yet
-	pass
+	print('hey this will do something')
+	velocity.x = 0
+	velocity.x += 200 if isRight() else -200
+	state = States.ROLLING
+	await get_tree().create_timer(0.5, false, false, true).timeout
+	state = States.IDLE
+
+func isRight():
+	return not $Sprite2D.flip_h
